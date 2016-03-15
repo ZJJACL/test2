290ca9c6943ff56880354346c091e0c826405d90
ZJJACL <1152512286@qq.com>
2016-03-15 21:26:50 +0800

    ffff

--- src/com/example/String/AppContent.java
+++ src/com/example/String/AppContent.java
@@ -4,7 +4,7 @@ public class AppContent {
 
 	public static final String IP = "10.1.161.37";
 	public static float packet = 100;
-	public static String buyeraccount = "";
+	public static String buyeraccount = "dddddddd";
 	public static String buyerpassword="ffffffff";
 
 }
